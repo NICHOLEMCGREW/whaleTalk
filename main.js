501582fb38662e06e4c4e01bc4d9a0f6974930d7
Nichole McGrew <nicholemcgrew@gmail.com>
2022-07-26 15:15:14 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let input = "Time flies when you're having fun";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+    // console.log(`inputIndex is ${inputIndex}.`)
+    if (input[inputIndex] === 'e') {
+      resultArray.push(input[inputIndex]);
+    }
+    if (input[inputIndex] === 'u') {
+        resultArray.push(input[inputIndex]);
+      }
+    
+  for (let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++) {
+    // console.log(`inputIndex is ${vowelIndex}.`)
+
+    if (input[inputIndex] === vowels[vowelIndex]) {
+        //   console.log(input[inputIndex]);
+          resultArray.push(input[inputIndex]);
+    }
+  }
+}
+// console.log(resultArray);
+
+const resultString = resultArray.join(' ').toUpperCase();
+console.log(resultString)
